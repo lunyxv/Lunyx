975a9436f17156503b8b4fa4a8143948abf747ca
luny <luarmorv2@gmail.com>
2026-08-11 01:58:31 -0400

    Update latest.lua

--- versions/latest.lua
+++ versions/latest.lua
@@ -2,10 +2,7 @@ return {
     ["0.0.1"] = {
         date = "08/11/2026",
         changelog = {
-            "[+] Building the bot",
-            "[+] Obfuscation engine integrated",
-            "[+] Slash command support",
-            "[+] File upload handling"
+            "[+] Building the bot"
         }
     }
 }
